ad7de9bb44f10078086ac0dac373f588dbcc4df3
Rifat3706 <rokibulhasnatrifat3706@gmail.com>
2021-09-20 05:37:11 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+const API_KEY = `ee2c00644087e34b577071c9e3aad043`;
+const searchTemperature = () => {
+    const city = document.getElementById('city-name').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayTemperature(data));
+}
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temp', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main)
+    // set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
+}
